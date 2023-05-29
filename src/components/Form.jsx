5e62d77459b44da4165c4f63404b020dcb0a2e3e
Axel Torrico <torricohernan2@gmail.com>
2023-05-29 17:15:57 -0300

    utilice react-form para validar el formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,11 @@
 import React, { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
 import '../styles/Form.css'
 
 export function Form() {
   const form = useRef();
+  const { register, formState: { errors }, handleSubmit } = useForm()
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -17,18 +19,35 @@ export function Form() {
   };
 
   return (
-    <form className='Form' ref={form} onSubmit={sendEmail}>
+    <form className='Form' ref={form} onSubmit={handleSubmit(sendEmail)}>
       <div className='Form-content'>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input
+          className={`input ${errors.user_name?.type === 'required' ? 'error' : ''}`}
+          type="text"
+          name="user_name"
+          {...register('user_name', { required: true })}
+        />
+        {errors.user_name?.type === 'required' && <p>Write your name</p>}
       </div>
       <div className='Form-content'>
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input
+          className={`input ${errors.user_email?.type === 'required' ? 'error' : ''}`}
+          type="email"
+          name="user_email"
+          {...register('user_email', { required: true })}
+        />
+        {errors.user_email?.type === 'required' && <p>Write your email</p>}
       </div>
       <div className='Form-content'>
         <label>Message</label>
-        <textarea className='input-text' name="message" />
+        <textarea
+          className={`input-text ${errors.message?.type === 'required' ? 'error' : ''}`}
+          name="message"
+          {...register('message', { required: true })}
+        />
+        {errors.message?.type === 'required' && <p>You must write a message</p>}
       </div>
       <input className='Form-button' type="submit" value="Send" />
     </form>
