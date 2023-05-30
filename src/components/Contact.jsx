2c315bc0549523e6286f593fd686508aa9a418db
Axel Torrico <torricohernan2@gmail.com>
2023-05-30 10:42:19 -0300

    agregue contenedor para el texto y el formulario

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,6 +12,8 @@ export function Contact() {
         <h2>
           <AiFillPhone className="contact-icon"/> Contact me
         </h2>
+      </div>
+      <div className="footer-container-content">
         <p className="footer-container-text">Write your message here or talk to me on my social media.</p>
         <Form />
       </div>
